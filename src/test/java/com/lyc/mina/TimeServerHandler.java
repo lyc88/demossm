e6d940c3b0b578af8c46c4901af2221df0ffca40
lyc88 <984006207@qq.com>
2017-06-12 18:28:48 +0800

    mina入门

---
+++ src/test/java/com/lyc/mina/TimeServerHandler.java
@@ -0,0 +1,37 @@
+package com.lyc.mina;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+import java.util.Date;
+
+/**
+ * Created by lyc on 2017/6/12.
+ */
+public class TimeServerHandler extends IoHandlerAdapter {
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause)
+            throws Exception {
+        cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message)
+            throws Exception {
+        String str = message.toString();
+        System.out.println("Message read:");
+        System.out.println(str);
+
+        Date date = new Date();
+        session.write(date.toString());
+        System.out.println("Message written...");
+        session.close();
+    }
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status)
+            throws Exception {
+        System.out.println("IDLE " + session.getIdleCount(status));
+    }
+}
