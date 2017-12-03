d060773158d05bb511a82cac3d2a002f0c3e42ae
lyc <lyc@metadata.net.cn>
2017-12-03 16:48:31 +0800

    添加 原生sockt1

--- src/main/java/com/lyc/web/IndexController.java
+++ src/main/java/com/lyc/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.lyc.web;
 
+import com.lyc.dao.FormModel;
+import com.lyc.model.po.TestDate;
 import com.lyc.service.BService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.constraints.Digits;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,7 +21,18 @@ public class IndexController {
     @Autowired
     private BService bService;
     @RequestMapping("hello")
-    public @ResponseBody List hello(){
+    public @ResponseBody List hello(@FormModel String a){
+        System.out.println(a);
+
        return bService.findList();
+    }
+
+    @RequestMapping("hello1")
+    public @ResponseBody TestDate hello1(){
+        TestDate testDate =new TestDate();
+        testDate.setB(new Date());
+        testDate.setName("hello");
+
+        return testDate;
     }
 }
